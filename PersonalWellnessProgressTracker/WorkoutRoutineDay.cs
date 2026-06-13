d0c1d633b148bc490af3459796bafdf7db2f0eda
jeffohara78 <85190271+jeffohara78@users.noreply.github.com>
2026-06-13 13:46:59 -0700

    Add files via upload

---
+++ PersonalWellnessProgressTracker/WorkoutRoutineDay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PersonalWellnessProgressTracker
+{
+    public class WorkoutRoutineDay
+    {
+        public int RoutineId { get; set; }
+
+        public int ProfileId { get; set; }
+
+        public DayOfWeek PlannedDay { get; set; }
+
+        public string PlannedWorkoutType { get; set; }
+
+        public int PlannedMinutes { get; set; }
+
+        public WorkoutRoutineDay()
+        {
+        }
+
+        // NEW 06/13/2026:
+        // Stores a planned workout day for one specific profile.
+        public WorkoutRoutineDay(int routineId, int profileId, DayOfWeek plannedDay, string plannedWorkoutType, int plannedMinutes)
+        {
+            RoutineId = routineId;
+            ProfileId = profileId;
+            PlannedDay = plannedDay;
+            PlannedWorkoutType = plannedWorkoutType;
+            PlannedMinutes = plannedMinutes;
+        }
+    }
+}
